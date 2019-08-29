f53e39e355d0d235f650704aafe47dec35bf0068
Suman Kumar <suman845415@gmail.com>
2019-08-29 20:04:00 +0530

    Add card with firebase database

---
+++ src/components/Practice/AddCard/AddCard.js
@@ -0,0 +1,119 @@
+import React, { Component } from "react";
+import "./AddCard.scss";
+import firebase from "../../../firebase";
+
+class AddCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      currentItem: "",
+      items: []
+    };
+  }
+
+  componentDidMount() {
+    const itemsRef = firebase.database().ref("items");
+    itemsRef.on("value", myItem => {
+      let items = myItem.val();
+      let newItem = [];
+      for (let item in items) {
+        newItem.push({
+          id: item,
+          title: items[item].title,
+          user: items[item].user
+        });
+      }
+      this.setState({ items: newItem });
+    });
+  }
+
+  handleChangefield = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  handleAddCard = event => {
+    event.preventDefault();
+    const { username, currentItem } = this.state;
+    const itemsRef = firebase.database().ref("items");
+    const item = {
+      user: username,
+      title: currentItem
+    };
+    itemsRef.push(item);
+    this.setState({
+      username: "",
+      currentItem: ""
+    });
+  };
+
+  removeItem = itemId => {
+    const itemRef = firebase.database().ref(`/items/${itemId}`);
+    itemRef.remove();
+  };
+
+  render() {
+    const { items, username, currentItem } = this.state;
+    return (
+      <div className="add-card-conteiner">
+        <div className="add-card-header">
+          <h3>Add Card Demo</h3>
+        </div>
+        <div className="add-card-body">
+          <section className="add-item">
+            <form onSubmit={this.handleAddCard} className="text-center">
+              <input
+                className="form-control"
+                type="text"
+                name="username"
+                placeholder="What's your name?"
+                value={username}
+                onChange={this.handleChangefield}
+              />
+              <input
+                className="form-control mt-2"
+                type="text"
+                name="currentItem"
+                placeholder="What are you bringing?"
+                value={currentItem}
+                onChange={this.handleChangefield}
+              />
+              <button
+                className="btn btn-success mt-3"
+                disabled={username === "" || currentItem === ""}
+              >
+                Add Item
+              </button>
+            </form>
+          </section>
+          <section className="display-item">
+            <div className="wrapper">
+              <ul>
+                {items.map(item => {
+                  const { id, title, user } = item;
+                  return (
+                    <li key={id}>
+                      <h4>{user}</h4>
+                      <p>{title}</p>
+                      <hr />
+                      <button
+                        className="btn btn-info btn-sm"
+                        onClick={() => this.removeItem(id)}
+                      >
+                        Remove Item
+                      </button>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          </section>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default AddCard;
--- src/layout/MyRouter.js
+++ src/layout/MyRouter.js
@@ -1,49 +1,56 @@
-import React, { Suspense, lazy } from 'react';
-import { BrowserRouter, Switch, Route, Redirect, withRouter } from 'react-router-dom';
+import React, { Suspense, lazy } from "react";
+import { BrowserRouter, Switch, Route, withRouter } from "react-router-dom";
 
-const SamplePage = withRouter(
-  lazy(() => import('../components/Home')),
-);
+const SamplePage = withRouter(lazy(() => import("../components/Home")));
 
 const CountAsString = withRouter(
-  lazy(() => import('../components/Project/CountAsString')),
+  lazy(() => import("../components/Project/CountAsString"))
 );
 
 const ViewPortHeightWidth = withRouter(
-  lazy(() => import('../components/Practice/ViewPortHeightWidth')),
+  lazy(() => import("../components/Practice/ViewPortHeightWidth"))
 );
 
 const Accordion = withRouter(
-  lazy(() => import('../components/Practice/Accordion')),
+  lazy(() => import("../components/Practice/Accordion"))
 );
 
 const CardEmoji = withRouter(
-  lazy(() => import('../components/Practice/CardEmoji')),
+  lazy(() => import("../components/Practice/CardEmoji"))
 );
 
 const ListCard = withRouter(
-  lazy(() => import('../components/Practice/ListCard')),
+  lazy(() => import("../components/Practice/ListCard"))
 );
 
 const LabelInput = withRouter(
-  lazy(() => import('../components/Practice/LabelInput'))
+  lazy(() => import("../components/Practice/LabelInput"))
+);
+
+const AddCard = withRouter(
+  lazy(() => import("../components/Practice/AddCard"))
 );
 
 const MyRouter = () => {
   return (
     <BrowserRouter>
-      <Suspense fallback='Suman'>
+      <Suspense fallback="Suman">
         <Switch>
           <Route exact path="/" component={SamplePage} />
           <Route exact path="/count-as-string" component={CountAsString} />
-          <Route exact path="/viewport-height-width" component={ViewPortHeightWidth} />
+          <Route
+            exact
+            path="/viewport-height-width"
+            component={ViewPortHeightWidth}
+          />
           <Route exact path="/accordion" component={Accordion} />
           <Route exact path="/card-emoji" component={CardEmoji} />
           <Route exact path="/list-card" component={ListCard} />
           <Route exact path="/label-input" component={LabelInput} />
+          <Route exact path="/add-card" component={AddCard} />
         </Switch>
       </Suspense>
     </BrowserRouter>
-  )
-}
+  );
+};
 export default MyRouter;
