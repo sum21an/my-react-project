19c6f7324d062d2859524c3c0fd24dc18708fdf0
Suman Kumar <suman845415@gmail.com>
2019-08-30 14:50:22 +0530

    Place holder name changed

--- src/components/Practice/AddCard/AddCard.js
+++ src/components/Practice/AddCard/AddCard.js
@@ -68,7 +68,7 @@ class AddCard extends Component {
                 className="form-control"
                 type="text"
                 name="username"
-                placeholder="What's your name?"
+                placeholder="Enter title"
                 value={username}
                 onChange={this.handleChangefield}
               />
@@ -76,7 +76,7 @@ class AddCard extends Component {
                 className="form-control mt-2"
                 type="text"
                 name="currentItem"
-                placeholder="What are you bringing?"
+                placeholder="Enter subject content"
                 value={currentItem}
                 onChange={this.handleChangefield}
               />
